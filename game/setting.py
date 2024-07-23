c2dcd18f8391511979e55d74fa6250517d5f2cdb
Heavenell <Fauzirahmadiariateja@gmail.com>
2024-07-23 17:21:06 +0200

    Create setting.py

---
+++ game/setting.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Setting:
+    def __init__(self):
+        self.update_display_info()
+
+    def update_display_info(self):
+        display_info = pygame.display.Info()
+        self.screen_width = display_info.current_w
+        self.screen_height = display_info.current_h
+
+    def get_dimensions(self):
+        return self.screen_width, self.screen_height
+
+    def resize_display(self, width, height):
+        self.screen_width = width
+        self.screen_height = height
+        pygame.display.set_mode((self.screen_width, self.screen_height), pygame.RESIZABLE)
