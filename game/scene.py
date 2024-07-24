f226c5ab59a2051b26cb2b3e2dd674a79ff07f81
Heavenell <Fauzirahmadiariateja@gmail.com>
2024-07-24 23:26:55 +0200

    DialogBox size is now according to screen size

--- game/scene.py
+++ game/scene.py
@@ -1,17 +1,27 @@
-import pygame
 from game.dialogbox import DialogueBox
 
 class Scene:
     def __init__(self, setting):
         self.setting = setting
-        self.dialogue_box = None
         self.background = None
-        self.init_dialogueBox()
 
-    def init_dialogueBox(self):
-        self.dialogue_box = DialogueBox(50, 700, self.setting.get_dimensions()[0]-100, self.setting.get_dimensions()[1]-750, (100, 100, 100))
+        self.dialogue_box = None
+        self.dialogue_box_background = (255, 255, 255)
+        self.dialogue_box_x = int(5*self.setting.multiplier)
+        self.dialogue_box_y = int(70*self.setting.multiplier)
+        self.dialogue_box_width = self.setting.get_dimensions()[0]- int(10*self.setting.multiplier)
+        self.dialogue_box_height = self.setting.get_dimensions()[1]- int(75*self.setting.multiplier)
+        self.init_dialogue_box()
+
+    def init_dialogue_box(self):
+        self.dialogue_box = DialogueBox(self.dialogue_box_x, self.dialogue_box_y, self.dialogue_box_width, self.dialogue_box_height, self.dialogue_box_background, self.setting)
+
+        # for testing purposes
         self.dialogue_box.set_name("Character Name")
-        self.dialogue_box.set_dialogue("This is a sample dialogue for testing the DialogueBox.")
+        self.dialogue_box.set_dialogue("This is a sample dialogue for testing the DialogueBox. A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A A ")
+    
+    def set_background(self, background):
+        self.background = background
 
     def draw(self, screen):
         self.dialogue_box.draw(screen)
