bfa3300b4a1bf6cebee85015d5c9a558061e3134
Heavenell <Fauzirahmadiariateja@gmail.com>
2024-07-26 16:41:22 +0200

    add list of valid emotion

--- game/character/emotion.py
+++ game/character/emotion.py
@@ -5,7 +5,27 @@ class Emotion:
     A class to represent an emotion of a character.
     Emotion contains an image that represents the emotion.
     """
-    def __init__(self, name, emotion_id):
+    EMOTIONS = {
+        0: "neutral",
+        1: "happy",
+        2: "sad",
+        3: "angry",
+        4: "surprised",
+        5: "disgusted",
+        6: "scared"
+    }
+
+    def __init__(self, name=None, emotion_id=None):
+        if name is None and emotion_id is None:
+            print("Emotion needs a name or emotion ID")
+            return
+        if emotion_id is not None and emotion_id not in self.EMOTIONS:
+            print(f"Emotion ID {emotion_id} does not exist")
+            return
+        if name is not None and name not in self.EMOTIONS.values():
+            print(f"Emotion name '{name}' does not exist")
+            return
+
         self.name = name
         self.emotion_id = emotion_id
         self.image = None
