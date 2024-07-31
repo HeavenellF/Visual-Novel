38c2f390b7fbbd13659ccda1831a1d05126ad29a
Heavenell <Fauzirahmadiariateja@gmail.com>
2024-08-01 00:14:26 +0200

    Create button.py

---
+++ game/ui/button.py
@@ -0,0 +1,25 @@
+import pygame
+
+class Button:
+
+    instances = []
+
+    def __init__(self, name, x, y, image, action=None, setting=None):
+        self.name = name
+        self.image = image
+        self.rect = image.get_rect()
+        self.rect.topleft = (x, y)
+        self.action = action
+        self.setting = setting
+
+        Button.instances.append(self)
+
+    def draw(self, screen):
+        font = pygame.font.SysFont(self.setting.instance.font, int(2*self.setting.multiplier))
+        text_surf  = font.render(self.name, True, (0,0,0))
+        text_rect = text_surf.get_rect(center = self.rect.center)
+        screen.blit(self.image, self.rect)
+        screen.blit(text_surf, text_rect)
+        
+    def handle(self):
+        self.action()
