2fc5184a1f422ac52e7eb41fbcd55f799575352c
Heavenell <Fauzirahmadiariateja@gmail.com>
2024-08-09 22:10:41 +0200

    move button image to menu_item

--- game/menu.py
+++ game/menu.py
@@ -37,31 +37,29 @@ class Menu:
             self.check_submenu(a, menu_item)
 
     def check_submenu(self, a, menu_item):
-        button_image = pygame.image.load("resources/images/button.png").convert_alpha()
-        button_image = pygame.transform.scale(button_image, (200, 50))
         
         prev_menu_item = menu_item
         new_menu_item = MenuItem(a["name"], prev_menu_item, self.setting)
         self.menu_items.append(new_menu_item)
 
         if a.get("submenu") is not None:
-            prev_menu_item.add_button(a["name"], button_image, lambda: self.change_menu(new_menu_item))
+            prev_menu_item.add_button(a["name"], lambda: self.change_menu(new_menu_item))
             submenus = a["submenu"]
             for submenu in submenus:
                 self.check_submenu(submenu, new_menu_item)
 
         elif a.get("function") is not None:
             function = getattr(self, a["function"], None)
-            prev_menu_item.add_button(a["name"], button_image, function)
+            prev_menu_item.add_button(a["name"], function)
 
         elif a.get("scenes") is not None:
-            prev_menu_item.add_button(a["name"], button_image, lambda: self.change_menu(new_menu_item))
+            prev_menu_item.add_button(a["name"], lambda: self.change_menu(new_menu_item))
             scenes = a["scenes"]
             for scene in scenes:
-                new_menu_item.add_button(scene["name"], button_image, None)
+                new_menu_item.add_button(scene["name"], None)
         
         else:
-            prev_menu_item.add_button(a["name"], button_image, lambda: self.change_menu(new_menu_item))
+            prev_menu_item.add_button(a["name"], lambda: self.change_menu(new_menu_item))
 
     def change_menu(self, menu):
         self.current_menu = menu
@@ -82,9 +80,11 @@ class MenuItem:
         self.setting = setting
         self.x = int(setting.screen_width / 7)
         self.y = int(setting.screen_height / 4)
+        self.button_image = pygame.image.load("resources/images/button.png").convert_alpha()
+        self.button_image = pygame.transform.scale(self.button_image, (400, 50))
 
-    def add_button(self, name, image, function=None):
-        button = Button(name, self.x, self.y, image, function, self.setting)
+    def add_button(self, name, function=None):
+        button = Button(name, self.x, self.y, self.button_image, function, self.setting)
         self.y += int(15*self.setting.multiplier)
         self.buttons.append(button)
 
