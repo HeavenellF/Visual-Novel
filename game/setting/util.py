f05a8cf18faa27d242ba2d13de4e5ee1ca3be81e
Heavenell <Fauzirahmadiariateja@gmail.com>
2024-08-10 22:14:13 +0200

    Update util.py

--- game/setting/util.py
+++ game/setting/util.py
@@ -1,5 +1,10 @@
 import json
+import os
 
 def read_json_file(path):
+    if not os.path.exists(path):
+        print(f"File not found: {path}")
+        return None
+
     with open(path, 'r', encoding="utf-8") as file:
         return json.load(file) 
